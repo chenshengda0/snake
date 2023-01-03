f2ce4a4dacdc4c737699c898437a3342b7947c86
shengdachen1991 <shengdachen1991@gmail.com>
2023-01-03 16:23:10 +0800

    config webpack、bable

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+function test<T>(a:T):T{
+    return a;
+}
+console.log( test(123) )
+
+const obj = {name: "孙悟空" ,age: 19}
+console.log( obj )
+obj.age = 18;
+console.log( obj )
+console.log( Promise )
+const tests = (a:number,b:number) => a+b;
+console.log(tests(1,2))
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require("path")
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+const TerserPlugin = require('terser-webpack-plugin');
+module.exports = {
+    //指定入口文件
+    entry: "./src/index.ts",
+    //指定输出文件
+    output: {
+        path: path.resolve(__dirname,"Install/build"),
+        filename: "bundle.js",
+        environment: {
+            arrowFunction: false,
+        }
+    },
+    mode: "development",
+    //mode: "production",
+    //devtool: 'eval-source-map',
+    //指定webpack打包时要使用的模块
+    module: {
+        //指定要加载的规则
+        rules: [
+            //ts 配置
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        //指定加载器
+                        loader: "babel-loader",
+                        options: {
+                            //设置预定义环境
+                            "presets": [
+                                [
+                                    "@babel/preset-env", 
+                                    {
+                                        "targets": {
+                                            "chrome": "58",
+                                            "ie": "11"
+                                        },
+                                        "useBuiltIns": "usage",
+                                        "corejs": "3"
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    "ts-loader"
+                ],
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    plugins: [
+        //设置网页模版
+        new HTMLWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        //报错
+        new webpack.IgnorePlugin({
+            resourceRegExp: /^electron$/
+        }),
+    ],
+    //设置哪些文件可以作为模块引用
+    resolve: {
+        extensions: ['.js', '.json', '.ts'],
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({
+            extractComments: false,
+        })],
+    },
+    performance : {
+        hints : false
+    }
+}
